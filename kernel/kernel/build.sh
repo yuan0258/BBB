f1dbade28aee7a7680c8733ec357643a328ce5db
CyunYuan Chen <yuan0258@gmail.com>
2014-09-12 15:31:34 +0800

    Upload prebuild image and modify build script

--- kernel/kernel/build.sh
+++ kernel/kernel/build.sh
@@ -5,5 +5,8 @@ export PATH=$PATH:/home/yuan/BBB/BBB/u-boot-2013.07/tools
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage dtbs
 #This step builds the kernel for the BBB
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- uImage-dtb.am335x-boneblack
+#Copy image
+cp arch/arm/boot/uImage-dtb.am335x-boneblack ../../images/
 #This builds the kernel modules and may take 20 minutes or longer
 #make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- modules
+
