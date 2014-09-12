64e8ad5635091fe3e09ab54879a61dbdad3f6639
CyunYuan Chen <yuan0258@gmail.com>
2014-09-12 15:27:59 +0800

    Modify download kernel code script

--- kernel/download_kernel.sh
+++ kernel/download_kernel.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+git clone git://github.com/beagleboard/kernel.git
+git check 3.8
+./patch
 cp configs/beaglebone kernel/arch/arm/configs/beaglebone_defconfig
 #Download pm image
 wget http://arago-project.org/git/projects/?p=am33x-cm3.git\;a=blob_plain\;f=bin/am335x-pm-firmware.bin\;hb=HEAD -O kernel/firmware/am335x-pm-firmware.bin
+cd kernel
+sh build.sh
